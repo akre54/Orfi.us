6f68df673b4dbfc54cec59d76c86e91cb630b7ce
djangoboots <blackmore.stephen@gmail.com>
2012-02-25 23:30:37 -0500

    Update fbquery.php

--- fbquery.php
+++ fbquery.php
@@ -2,10 +2,16 @@
 
 
 require_once('includes/db.php');
-
+$fb_id = $_GET['fb_id'];
 
 $db = new mysql();
 
+$fb_auth = $db->get('users','fb_auth',"fb_id='$fb_id'");
+
+$graph_url = "https://graph.facebook.com/" . $fb_id . "?access_token=" . $fb_auth;
+
+$user = json_decode(file_get_contents($graph_url));
+echo("Hello " . $user->name); 
 
 
 ?>
