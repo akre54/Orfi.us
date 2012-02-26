98d29089356614566c1ada49a915181e295b5d80
akre54 <krebsadam@gmail.com>
2012-02-26 01:13:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -26,6 +26,37 @@ if (isset($_GET['name'])) {
 <html>
 <head>
 <title>Check into <?= $name ?></title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+ <script> 
+ 	 var appID = "279836385418317";
+ 
+     function AddUser(user) {
+       $.post("get.php?type=adduser", user);
+     }
+
+     if (window.location.hash.length == 0) {
+       var path = 'https://www.facebook.com/dialog/oauth?';
+   var queryParams = ['client_id=' + appID,
+     'redirect_uri=' + window.location,
+     'response_type=token'];
+
+	var query = queryParams.join('&');
+	var url = path + query;
+	window.open(url);
+     } else {
+	var accessToken = window.location.hash.substring(1);
+	var path = "https://graph.facebook.com/me?";
+   	var queryParams = [accessToken, 'callback=addUser'];
+  	var query = queryParams.join('&');
+   	var url = path + query;
+
+	// use jsonp to call the graph
+	var script = document.createElement('script');
+	script.src = url;
+	document.body.appendChild(script);        
+    }
+</script> 
+
 </head>
 <body>
 	<h1><?= $name ?></h1>
